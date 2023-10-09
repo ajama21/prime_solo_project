453953a7a03fc9b6859b5fde6a3cc45b96635d66
Abdirahman Jama <mustalid21@gmail.com>
2023-10-09 11:08:20 -0500

    created necessary components and routes for project

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -6,16 +6,11 @@ import './LandingPage.css';
 import RegisterForm from '../RegisterForm/RegisterForm';
 
 function LandingPage() {
-  const [heading, setHeading] = useState('Welcome');
-  const history = useHistory();
-
-  const onLogin = (event) => {
-    history.push('/login');
-  };
+  
 
   return (
     <div className="container">
-      <h2>{heading}</h2>
+      <h2>Welcome</h2>
 
       <div className="grid">
         <div className="grid-col grid-col_8">
@@ -50,16 +45,6 @@ function LandingPage() {
             non fermentum mauris. Sed in enim ac turpis faucibus pretium in sit
             amet nisi.
           </p>
-        </div>
-        <div className="grid-col grid-col_4">
-          <RegisterForm />
-
-          <center>
-            <h4>Already a Member?</h4>
-            <button className="btn btn_sizeSm" onClick={onLogin}>
-              Login
-            </button>
-          </center>
         </div>
       </div>
     </div>
---
+++ src/components/Onboarding/Onboarding.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Onboarding() {
+  return (
+    <div><h1>Onboarding</h1></div>
+  )
+}
