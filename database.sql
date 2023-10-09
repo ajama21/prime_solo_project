c40a0a18844a960036ffce787aaf7b7530a31309
Abdirahman Jama <mustalid21@gmail.com>
2023-10-09 11:05:32 -0500

    create sql statement

--- database.sql
+++ database.sql
@@ -1,10 +1,61 @@
-
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
-CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+CREATE TABLE "public.driver" (
+	"id" serial NOT NULL,
+	"application_link" varchar NOT NULL,
+	"license_link" varchar NOT NULL,
+	"dot_link" varchar NOT NULL,
+	"company_policy_link" varchar NOT NULL,
+	"drug_alcohol_link" integer NOT NULL,
+	CONSTRAINT "driver_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
 );
+
+
+
+CREATE TABLE "public.truck" (
+	"id" serial NOT NULL,
+	"make" varchar NOT NULL,
+	"year" integer NOT NULL,
+	"model" varchar NOT NULL,
+	"image_link" varchar NOT NULL,
+	CONSTRAINT "truck_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.driver_truck" (
+	"id" serial NOT NULL,
+	"driver_id" integer NOT NULL,
+	"truck_id" integer NOT NULL,
+	CONSTRAINT "driver_truck_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.dispatcher" (
+	"id" serial NOT NULL,
+	"company_name" varchar NOT NULL,
+	"username" varchar NOT NULL UNIQUE,
+	"password" varchar NOT NULL,
+	"email" varchar NOT NULL UNIQUE,
+	CONSTRAINT "dispatcher_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+
+ALTER TABLE "driver_truck" ADD CONSTRAINT "driver_truck_fk0" FOREIGN KEY ("driver_id") REFERENCES "Driver"("id");
+ALTER TABLE "driver_truck" ADD CONSTRAINT "driver_truck_fk1" FOREIGN KEY ("truck_id") REFERENCES "Trucks"("id");
+
+
+
+
+
+
