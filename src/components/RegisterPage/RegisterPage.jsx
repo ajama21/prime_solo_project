bf752a196827a47214c9b550a3f02793b50cdb5b
Abdirahman Jama <mustalid21@gmail.com>
2023-10-10 00:46:40 -0500

    completed static element on register page

--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
-
-import { useHistory } from 'react-router-dom';
-import RegisterForm from '../RegisterForm/RegisterForm';
+import React from "react";
+import TopNav from "../Common/TopNav";
+import './Register.css'
 
 function RegisterPage() {
-
   return (
-    <div>
-      <h1>This is the Register Page</h1>
+    <div
+      className="container register_page"
+      style={{ backgroundImage: `url('/Freightliner.jpeg')` }}
+    >
+      <TopNav />
+      <form className="register_form">
+      <h1 className="welcome">Welcome</h1>
+        <div className="group">
+          <label htmlFor="companyname">Company Name</label>
+          <input
+            type="text"
+            name="companyname"
+            placeholder="Enter Company Name"
+          />
+        </div>
+        <div className="group">
+          <label htmlFor="username">User Name</label>
+          <input type="text" name="username" placeholder="Enter User Name" />
+        </div>
+        <div className="group">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" placeholder="Enter Password" />
+        </div>
+        <div className="group">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" placeholder="Enter Email" />
+        </div>
+        <button className="sign_up">Sign-Up</button>
+      </form>
     </div>
   );
 }
