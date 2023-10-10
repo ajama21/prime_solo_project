63d2e529796d3af1d8c08158bd1ed21f13c87b80
Abdirahman Jama <mustalid21@gmail.com>
2023-10-10 00:49:33 -0500

    landing page static elements complete

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,50 +1,26 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import './LandingPage.css';
-
-// CUSTOM COMPONENTS
-import RegisterForm from '../RegisterForm/RegisterForm';
+import React, { useState } from "react";
+import "./LandingPage.css";
+import TopNav from "../Common/TopNav";
+import { Link } from "react-router-dom";
 
 function LandingPage() {
-  
-
   return (
-    <div className="container">
-      <h2>Welcome</h2>
-
-      <div className="grid">
-        <div className="grid-col grid-col_8">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-            id felis metus. Vestibulum et pulvinar tortor. Morbi pharetra lacus
-            ut ex molestie blandit. Etiam et turpis sit amet risus mollis
-            interdum. Suspendisse et justo vitae metus bibendum fringilla sed
-            sed justo. Aliquam sollicitudin dapibus lectus, vitae consequat odio
-            elementum eget. Praesent efficitur eros vitae nunc interdum, eu
-            interdum justo facilisis. Sed pulvinar nulla ac dignissim efficitur.
-            Quisque eget eros metus. Vestibulum bibendum fringilla nibh a
-            luctus. Duis a sapien metus.
-          </p>
-
-          <p>
-            Praesent consectetur orci dui, id elementum eros facilisis id. Sed
-            id dolor in augue porttitor faucibus eget sit amet ante. Nunc
-            consectetur placerat pharetra. Aenean gravida ex ut erat commodo, ut
-            finibus metus facilisis. Nullam eget lectus non urna rhoncus
-            accumsan quis id massa. Curabitur sit amet dolor nisl. Proin
-            euismod, augue at condimentum rhoncus, massa lorem semper lacus, sed
-            lobortis augue mi vel felis. Duis ultrices sapien at est convallis
-            congue.
-          </p>
-
-          <p>
-            Fusce porta diam ac tortor elementum, ut imperdiet metus volutpat.
-            Suspendisse posuere dapibus maximus. Aliquam vitae felis libero. In
-            vehicula sapien at semper ultrices. Vivamus sed feugiat libero. Sed
-            sagittis neque id diam euismod, ut egestas felis ultricies. Nullam
-            non fermentum mauris. Sed in enim ac turpis faucibus pretium in sit
-            amet nisi.
-          </p>
+    <div
+      className="container"
+      style={{ backgroundImage: `url('/Freightliner.jpeg')` }}
+    >
+      <TopNav />
+      <div className="landingPage_image">
+        <div className="comment">
+          <p>“Navigate with Confidence. Fleet-Tracker in Your Hands!”</p>
+        </div>
+        <div className="hero_buttons">
+          <Link className="hero_button register" to="/register">
+            Get Started
+          </Link>
+          <Link className="hero_button login" to="/login">
+            Login
+          </Link>
         </div>
       </div>
     </div>
