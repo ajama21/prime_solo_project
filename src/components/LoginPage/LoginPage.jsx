af7625aeee73a50dd9a39ec9d7ccafecc1547ec1
Abdirahman Jama <mustalid21@gmail.com>
2023-10-10 00:47:24 -0500

    static elements on landing page complete with styling

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import LoginForm from '../LoginForm/LoginForm';
+import TopNav from '../Common/TopNav';
 
 function LoginPage() {
 
   return (
-    <div>
-      <h1>This is the login page</h1>
-    </div>
+    <div
+    className="container register_page"
+    style={{ backgroundImage: `url('/Freightliner.jpeg')` }}
+  >
+    <TopNav />
+    <form className="register_form">
+    <h1 className="welcome">Welcome</h1>
+      <div className="group">
+        <label htmlFor="username">User Name</label>
+        <input type="text" name="username" placeholder="Enter User Name" />
+      </div>
+      <div className="group">
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" placeholder="Enter Password" />
+      </div>
+
+      <button className="sign_up">Log-in</button>
+    </form>
+  </div>
   );
 }
 
