b9a578c85f9c76cbe7c7c50aa4ff048fe7ee08ac
Abdirahman Jama <mustalid21@gmail.com>
2023-10-11 00:40:35 -0500

    side menu nav component static element complete

--- src/components/Common/SideMenu.jsx
+++ src/components/Common/SideMenu.jsx
@@ -1,9 +1,24 @@
+import { func } from 'prop-types'
 import React from 'react'
+import { Link } from 'react-router-dom';
 
-export default function SideMenu() {
+export default function SideMenu({ close }) {
   return (
-    <div>
-        <h1>SideMenu</h1>
+    <div className='sideMenu' onClick={(e) => e.target === e.currentTarget ? close() : null}>
+      <div className='sideMenuOptions'>
+        <Option text='Dashboard' to='/dashboard'/>
+        <Option text='Add New Driver' to='/dashboard/onboarding'/>
+        <Option text='View All Drivers' to='/dashboard'/>
+        <Option text='Add New Truck' to='/dashboard/newtruck'/>
+        <Option text='View All Trucks' to='/dashboard'/>
+        <button>Sign Out</button>
+      </div>
     </div>
+  )
+}
+
+function Option({ text,to }){
+  return (
+    <Link to={to}>{text}</Link>
   )
 }
