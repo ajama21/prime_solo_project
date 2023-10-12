d11ae49a87e8d50b42b292c9d2b7bf08f0705d14
Abdirahman Jama <mustalid21@gmail.com>
2023-10-11 21:57:13 -0500

    created form states for login and register components

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,28 +1,57 @@
-import React from 'react';
-import TopNav from '../Common/TopNav';
+import React, { useState } from "react";
+import TopNav from "../Common/TopNav";
 
 function LoginPage() {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const Submit = (e) => {
+    e.preventDefault();
+    console.log("submit button clicked");
+  };
 
   return (
     <div
-    className="container register_page"
-    style={{ backgroundImage: `url('/Freightliner.jpeg')` }}
-  >
-    <TopNav />
-    <form className="register_form">
-    <h1 className="welcome">Welcome</h1>
-      <div className="group">
-        <label htmlFor="username">User Name</label>
-        <input type="text" name="username" placeholder="Enter User Name" />
-      </div>
-      <div className="group">
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" placeholder="Enter Password" />
-      </div>
+      className="container register_page"
+      style={{ backgroundImage: `url('/Freightliner.jpeg')` }}
+    >
+      <TopNav />
+      <form className="register_form">
+        <h1 className="welcome">Welcome</h1>
+        <div className="group">
+          <label htmlFor="username">User Name</label>
+          <input
+            type="text"
+            name="username"
+            placeholder="Enter User Name"
+            required
+            minLength={3}
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </div>
+        <div className="group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter Password"
+            required
+            minLength={8}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
 
-      <button className="sign_up">Log-in</button>
-    </form>
-  </div>
+        <button
+          className="sign_up"
+          onClick={(e) => Submit(e)}
+          disabled={!userName || !password}
+        >
+          Log-in
+        </button>
+      </form>
+    </div>
   );
 }
 
--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import TopNav from "../Common/TopNav";
 import "./Register.css";
 
 function RegisterPage() {
+  const [companyName, setCompanyName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const Submit = (e) => {
+    e.preventDefault()
+    console.log("submit button clicked");
+  };
+
   return (
     <div
       className="container register_page"
@@ -17,21 +27,49 @@ function RegisterPage() {
             type="text"
             name="companyname"
             placeholder="Enter Company Name"
+            required
+            minLength={3}
+            value={companyName}
+            onChange={(e) => setCompanyName(e.target.value)}
           />
         </div>
         <div className="group">
           <label htmlFor="username">User Name</label>
-          <input type="text" name="username" placeholder="Enter User Name" />
+          <input
+            type="text"
+            name="username"
+            placeholder="Enter User Name"
+            required
+            minLength={3}
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
         </div>
         <div className="group">
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" placeholder="Enter Password" />
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter Password"
+            required
+            minLength={8}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <div className="group">
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" placeholder="Enter Email" />
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter Email"
+            required
+            minLength={3}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
-        <button className="sign_up">Sign-Up</button>
+        <button className="sign_up" onClick={(e)=> Submit(e)} disabled={!companyName || !userName || !password || !email}>Sign-Up</button>
       </form>
     </div>
   );
