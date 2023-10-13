9eb429dd6dba606509a276738d4a87c383e386d2
Abdirahman Jama <mustalid21@gmail.com>
2023-10-12 23:48:02 -0500

    deleted static codes and added dynamic code also made edits to split the view between truck and driver on dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,73 +1,101 @@
 import React, { useState } from "react";
 import "./Dashboard.css";
 import TopNav from "../Common/TopNav";
-import { useHistory } from "react-router-dom"
+import { useHistory } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+
 
 export default function Dashboard() {
-
   const history = useHistory();
-  const [activeTab, setActiveTab] = useState('drivers');
+  const { search } = useLocation();
 
-  const drivers = [
-    {
-      id: 0,
-      name: "Joe",
-      status: "Assigned",
-    },
-    {
-      id: 1,
-      name: "Liplan",
-      status: "Unassigned",
-    },
-    {
-      id: 2,
-      name: "AJ",
-      status: "Unassigned",
-    }
-  ];
+  const dispatch = useDispatch();
 
-  const trucks = [
-    {
-      id: 0,
-      make: "Freightliner",
-      truckNumber: 485,
-    },
-    {
-      id: 1,
-      make: "Volvo",
-      truckNumber: 200,
-    },
-  ];
+  const trucks = useSelector((store) => store.trucks);
+  const drivers = useSelector((store) => store.drivers);
+
+
+  useEffect(() => {
+    dispatch({ type: "FETCH_TRUCKS" });
+    dispatch({ type: "FETCH_DRIVERS" });
+
+  }, [dispatch]);
+
+
+
+  // if(!params.view) return null;
 
   return (
     <div className="dashboard">
       <TopNav />
       <div className="table">
         <div className="tabs">
-          <span onClick={()=> setActiveTab('drivers')} className={activeTab === 'drivers' ? 'activeTab' : 'inactiveTab'}>Drivers</span>
-          <span onClick={()=> setActiveTab('trucks')} className={activeTab === 'trucks' ? 'activeTab' : 'inactiveTab'}>Trucks</span>
+          <span
+            onClick={() =>
+              history.replace({
+                pathname: "/dashboard",
+                search: new URLSearchParams({ view: "drivers" }).toString(),
+              })
+            }
+            className={search.split('=')[1] === "drivers" ? "activeTab" : "inactiveTab"}
+          >
+            Drivers
+          </span>
+          <span
+            onClick={() =>
+              history.replace({
+                pathname: "/dashboard",
+                search: new URLSearchParams({ view: "trucks" }).toString(),
+              })
+            }
+            className={search.split('=')[1]=== "trucks" ? "activeTab" : "inactiveTab"}
+          >
+            Trucks
+          </span>
         </div>
         <table>
           <thead>
-            <th>Name</th>
-            <th>Status</th>
+            <th>{search.split('=')[1]=== "trucks" ? "Make" : "Driver Name"}</th>
+            <th>{search.split('=')[1]=== "trucks" ? "Driver Name" : "Truck Number"}</th>
             <th>Actions</th>
           </thead>
           <tbody>
-            {activeTab === 'drivers' && drivers.map((driver)=> (
-              <tr>
-                <td>{driver.name}</td>
-                <td>{driver.status}</td>
-                <td><button onClick={() => history.push('/dashboard/driverpage/' + driver.id )}>View</button> <button>Remove</button></td>
-              </tr>
-            ))}
-             {activeTab === 'trucks' && trucks.map((truck)=> (
-              <tr>
-                <td>{truck.make}</td>
-                <td>{truck.truckNumber}</td>
-                <td><button onClick={() => history.push('/dashboard/truckpage/' + truck.id )}>View</button> <button>Remove</button></td>
-              </tr>
-            ))}
+            {search.split('=')[1] === "drivers" &&
+              drivers.map((driver) => (
+                <tr key={driver.id}>
+                  <td>{driver.name}</td>
+                  <td>{driver.truck_number}</td>
+                  <td>
+                    <button
+                      onClick={() =>
+                        history.push("/dashboard/driverpage/" + driver.id)
+                      }
+                    >
+                      View
+                    </button>{" "}
+                    <button>Remove</button>
+                  </td>
+                </tr>
+              ))}
+            {search.split('=')[1] === "trucks" &&
+              trucks.map((truck) => (
+                <tr>
+                  <td>{truck.make}</td>
+                  <td>{truck.name}</td>
+                  <td>
+                    <button
+                      onClick={() =>
+                        history.push("/dashboard/truckpage/" + truck.id)
+                      }
+                    >
+                      View
+                    </button>{" "}
+                    <button>Remove</button>
+                  </td>
+                </tr>
+              ))}
           </tbody>
         </table>
       </div>
